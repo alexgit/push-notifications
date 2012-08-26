40428accf8655a9260459ca76497b544ab2807a3
alexgit <alex.tashev@gmail.com>
2012-08-26 14:36:02 +0100

    Got some very simple realtime updates between two users working.

--- RealTime/RealTime/EndPoints/RelayEndpoint.cs
+++ RealTime/RealTime/EndPoints/RelayEndpoint.cs
@@ -4,14 +4,80 @@ using System.Linq;
 using System.Web;
 using SignalR;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RealTime.Models;
 
 namespace RealTime.EndPoints
 {
     public class RelayEndpoint : PersistentConnection
-    {
+    {        
+        private static ConnectionLookup connectionLookup = new ConnectionLookup();
+
         protected override Task OnConnectedAsync(IRequest request, string connectionId)
-        {                                   
+        {
+            if (request.User.Identity.IsAuthenticated) 
+            {
+                var username = request.User.Identity.Name;
+
+                connectionLookup.Add(username, connectionId);
+            }
+                                
             return Connection.Broadcast("User " + connectionId + " has connected");
+        }
+
+        protected override Task OnDisconnectAsync(string connectionId)
+        {            
+            connectionLookup.Remove(connectionId);
+
+            return Connection.Broadcast("connection severed");
+        }
+
+        protected override Task OnReconnectedAsync(IRequest request, IEnumerable<string> groups, string connectionId)
+        {
+            if (request.User.Identity.IsAuthenticated)
+            {
+                var username = request.User.Identity.Name;
+
+                connectionLookup.Update(username, connectionId);
+            }
+            else 
+            {
+                connectionLookup.RemoveConnection(connectionId);
+            }
+
+            return Connection.Broadcast("User " + connectionId + " has connected");
+        }
+        
+        protected override Task OnReceivedAsync(IRequest request, string connectionId, string data)
+        {
+            if (data == "Greetings")
+            {
+                return Connection.Send(connectionId, "Hello there");
+            }
+            else if (data == "newthing")
+            {
+                return Connection.Broadcast(JsonConvert.SerializeObject(new Thing
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "New Thing",
+                    Time = "12:05",
+                    Content = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }));
+            }
+            else if (data.StartsWith("imhandling"))
+            {
+                var id = data.Split('/')[1];
+                return Connection.Broadcast(string.Format("someoneshandling/{0}", id));
+            }
+            else
+            {
+                return Task.Factory.StartNew(() => { });
+            }
         }
     }
 }
--- RealTime/RealTime/Scripts/realtime.js
+++ RealTime/RealTime/Scripts/realtime.js
@@ -1,14 +1,44 @@
 ﻿
+function Thing(title, time, content) {
+    this.title = title;
+    this.time = time;
+    this.content = content;
+}
+
+
 $(function () {
-    var connection = $.connection('/relay');
+    var connection = window.con = $.connection('/relay');
+
+    var viewModel = {
+        things: ko.observableArray(),
+        handle: function (thing) {
+            viewModel.things.remove(thing);
+            connection.send('imhandling/' + thing.Id);
+        }
+    };
 
     connection.start(function () {
         console.log("connection started!");
     });
 
     connection.received(function (data) {
-        console.log(data);
+        console.log('got response: ' + data);
+
+        if (data.indexOf('someoneshandling') === 0) {
+            var id = data.split('/')[1];
+            viewModel.things.remove(function (thing) {
+                return thing.Id == id;
+            });
+        } else {
+            viewModel.things.push(JSON.parse(data));
+        }
     });
 
     console.log(connection.id);
+
+
+    ko.applyBindings(viewModel);
 });
+
+
+
